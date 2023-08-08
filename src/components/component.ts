7c830d319bdf4ed0b0d3ecc45ce5a8d752e7dc81
kimbangto <kimbangto.525@gmail.com>
2023-08-08 20:17:31 +0900

    컴포넌트 삭제 구현

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,5 +1,6 @@
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
+  removeFrom(parent: HTMLElement): void;
 }
 
 // HTMLelement 생성을 캡슐화한 class
@@ -14,5 +15,12 @@ export class BaseComponent<T extends HTMLElement> implements Component {
 
   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     parent.insertAdjacentElement(position, this.element);
+  }
+
+  removeFrom(parent: HTMLElement): void {
+    if (parent !== this.element.parentElement) {
+      throw new Error('선택한 아이템을 찾을 수 없습니다!');
+    }
+    parent.removeChild(this.element);
   }
 }
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,7 +4,19 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+type OnCloseListener = () => void;
+
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
+  private closeListener?: OnCloseListener;
+
   constructor() {
     super(`<li class="page-item">
             <div class="page-item_controls">
@@ -15,7 +27,7 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
 
     const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
     closeBtn.onclick = () => {
-      this.element.remove();
+      this.closeListener && this.closeListener(); // closeListener가 있으면 closeListener를 호출
     };
   }
 
@@ -23,16 +35,25 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
     const container = this.element.querySelector('.page-item_body')! as HTMLElement;
     child.attachTo(container);
   }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
 
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      if (window.confirm('정말로 삭제하시겠습니까?')) {
+        item.removeFrom(this.element);
+      }
+    });
   }
 }
